974c39a0b8c06768cd876c3a90789623ccc5d4ea
AndrewO <andrewosborn101@gmail.com>
2019-04-05 18:21:44 -0500

    almost done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
   
     // Your port; if not 3306
-    port: 3306,
+    port: 3307,
   
     // Your username
     user: "root",
@@ -17,9 +17,17 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  showTable();
   userChoices();
 });
+function showTable(){
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+
+      });
+}
 
 function userChoices() {
     inquirer
@@ -35,22 +43,23 @@ function userChoices() {
                 message: "How many would you like to buy?"
             }
         ]).then(function(answer){
-            console.log(answer.productID);
-            var query = "SELECT item_id, stock_quantity FROM products WHERE ?";
+            var query = "SELECT item_id, stock_quantity, price, product_name FROM products WHERE ?";
             connection.query(query, { item_id: answer.productID }, function(err, res) {
-                console.log(res[0].stock_quantity);
+                console.log("\nItem: " + res[0].product_name)
+                console.log("stock quantity: " + res[0].stock_quantity);
+                console.log("price of item: $" + res[0].price);
 
                 if(answer.productQuantity > res[0].stock_quantity){
                     console.log("We don't have that many in our stock...")
+                    connection.end();
                 }
 
                 else if(answer.productQuantity <= res[0].stock_quantity){
                     //subtract answer.productQuantity from res[0].stock_quantity and update the answer as the new res[0].stock_quantity in the database
                     var newQuantity = res[0].stock_quantity - answer.productQuantity;
-                    console.log(newQuantity);
 
-                    console.log("Updating product quantity\n");
-                    var updateQuery = connection.query(
+                    console.log("\nUpdating product quantity\n");
+                    connection.query(
                         "UPDATE products SET ? WHERE ?",
                         [
                             {
@@ -61,12 +70,16 @@ function userChoices() {
                             }
                         ],
                     )
-                    console.log(updateQuery.sql);
+
+                    var receipt = answer.productQuantity * res[0].price;
+                            //find price of product using id and multiply it by answer.productQuantity
+                            console.log("your purchase costs $" + receipt);
                     connection.end();
                 }
                     
-                else{
+                else{  
                     console.log("Please enter a valid id and/or quantity")
+                    connection.end();
                 }
 
             })
